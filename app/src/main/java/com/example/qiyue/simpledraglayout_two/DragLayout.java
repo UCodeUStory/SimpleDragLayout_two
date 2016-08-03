a1890317ef71788424439c4db96f8d9a6b1ca878
UCodeUStory <qiyuekoon@foxmail.com>
2016-08-03 13:43:09 +0800

    添加根据百分比缩放功能
    添加根据百分比缩放功能，通过设置translationX实现覆盖式滑动

--- app/src/main/java/com/example/qiyue/simpledraglayout_two/DragLayout.java
+++ app/src/main/java/com/example/qiyue/simpledraglayout_two/DragLayout.java
@@ -1,6 +1,8 @@
 package com.example.qiyue.simpledraglayout_two;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
@@ -8,7 +10,10 @@ import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.Scroller;
+
+import com.nineoldandroids.view.ViewHelper;
 
 /**
  * Created by qiyue on 2016/7/29 0029.
@@ -58,12 +63,12 @@ public class DragLayout extends ViewGroup {
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         /**
-         * 摆放Menu的位置，强制摆放到刚好全部出去
+         * 摆放Menu的位置，强制摆放到刚好全部出去，然后调用ScrollBy移动整个布局
          *
          */
         mMenu.layout(-mMenuWidth, 0, 0, mScreenHeight);
         //摆放Content的位置
-       // mContent.layout(0, 0, mScreenWidth, mScreenHeight);
+        mContent.layout(0, 0, mScreenWidth, mScreenHeight);
     }
 
     @Override
@@ -83,6 +88,7 @@ public class DragLayout extends ViewGroup {
          */
         measureChild(mMenu,widthMeasureSpec,heightMeasureSpec);
         measureChild(mContent, widthMeasureSpec, heightMeasureSpec);
+       // mMenuWidth = mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
         mMenuWidth = mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
         mContentWidth = mContent.getLayoutParams().width = mScreenWidth;
         Log.i("qiyue",""+mMenu.getWidth());
@@ -111,38 +117,51 @@ public class DragLayout extends ViewGroup {
                  */
                 int dx = currentX - mLastX;
                 if (dx < 0){/**向左滑动**/
-                    //边界控制，如果Menu已经完全显示，再滑动的话
-                    //Menu左侧就会出现白边了,进行边界控制
+                    /**
+                     * 边界控制，如果Menu已经完全显示，再滑动的话 Menu左侧就会出现白边了,进行边界控制
+                     */
                     Log.i("qiyue","getScrollX()="+getScrollX());
                     if (getScrollX() + Math.abs(dx) >= 0) {
                         //直接移动到（0，0）位置，不会出现白边
                         scrollTo(0, 0);
                         mMenu.setTranslationX(0);
-                    } else {//Menu没有完全显示呢
-                        //其实这里dx还是-dx，大家不用刻意去记
-                        //大家可以先使用dx，然后运行一下，发现
-                        //移动的方向是相反的，那么果断这里加个负号就可以了
+                    } else {
+                        /**
+                         *  其实这里dx还是-dx，大家不用刻意去记
+                         大家可以先使用dx，然后运行一下，发现
+                         移动的方向是相反的，那么果断这里加个负号就可以了
+                         */
                         scrollBy(-dx, 0);
-                      //  mMenu.setTranslationX(2*(mMenuWidth+getScrollX())/3);
+                        /**
+                         * TranslationX 和margin 效果一样，这里动态的改变这个值，
+                         * 一开始显示0，当滑动等于宽度时变成三分之二，也就刚好吻合，
+                         */
+                        mMenu.setTranslationX(2*(mMenuWidth+getScrollX())/3);
+                        /**mContent垂直方向，根据百分比缩放**/
+                        scrollScaleY();
                     }
 
                 }else{/**向右滑动**/
-                    //边界控制，如果Content已经完全显示，再滑动的话
-                    //Content右侧就会出现白边了，进行边界控制
+                    /**边界控制，如果Content已经完全显示，再滑动的话*/
                     Log.i("qiyue","向右滑动getScrollX()="+getScrollX());
                     if (getScrollX() - dx <= -mMenuWidth) {
                         //直接移动到（-mMenuWidth,0）位置，不会出现白边
-                       // scrollTo(-mMenuWidth, 0);
-                     //   mMenu.setTranslationX(0);
-                    } else {//Content没有完全显示呢
-                        //根据手指移动
+                        scrollTo(-mMenuWidth, 0);
+                        mMenu.setTranslationX(0);
+
+                    } else {
                         /**
-                         *     整体移动，同时移动改变Menu位置
+                         * 整体移动，同时移动改变Menu位置，本身dx是个矢量带方向，因此这里不用变
                          */
-
                         scrollBy(-dx, 0);
-                      //  mMenu.setTranslationX(2*(getScrollX())/2);
-
+                        /**
+                         * TranslationX 和margin 效果一样，这里动态的改变这个值，
+                         * 一开始显示三分之二，当滑动等于宽度时变成0，也就刚好吻合，
+                         * 这样有一种并非一起动，而是覆盖的感觉
+                         */
+                        mMenu.setTranslationX(2*(mMenuWidth+getScrollX())/3);
+                        /**mContent垂直方向，根据百分比缩放**/
+                        scrollScaleY();
                     }
 
                 }
@@ -153,34 +172,39 @@ public class DragLayout extends ViewGroup {
 
             case MotionEvent.ACTION_UP:
                 Log.i("qiyue","getScrollX()="+getScrollX()+"mMenuWidth="+mMenuWidth);
-                if (!isOpen) {
+               if (!isOpen) {
                     if (getScrollX() < -mMenuWidth / 4) {//打开Menu
-                        //调用startScroll方法，第一个参数是起始X坐标，第二个参数
-                        //是起始Y坐标，第三个参数是X方向偏移量，第四个参数是Y方向偏移量
+                      /**
+                         * 调用startScroll方法移动，-mMenuWidth-getScrollx() 是剩余滑动距离
+                         */
                         mScroller.startScroll(getScrollX(), 0, -mMenuWidth - getScrollX(), 0, 300);
-                        //设置一个已经打开的标识，当实现点击开关自动打开关闭功能时会用到
+                        /**设置一个已经打开的标识，当实现点击开关自动打开关闭功能时会用到**/
                         isOpen = true;
-                        //一定不要忘了调用这个方法重绘，否则没有动画效果
+                        /**一定不要忘了调用这个方法重绘，否则没有动画效果**/
+                        scrollEndScaleY();
                         invalidate();
-                    } else {//关闭Menu
-                        //同上
+                    } else {
+                        /**
+                         * 条件未达到，回弹
+                         */
                         mScroller.startScroll(getScrollX(), 0, -getScrollX(), 0, 300);
                         isOpen = false;
+                        scaleReset();
                         invalidate();
                     }
                 }else{
-                    if (getScrollX() < (-mMenuWidth / 1)+(mMenuWidth/4)) {//打开Menu
-                        //调用startScroll方法，第一个参数是起始X坐标，第二个参数
-                        //是起始Y坐标，第三个参数是X方向偏移量，第四个参数是Y方向偏移量
+                    if (getScrollX() < (-mMenuWidth / 1)+(mMenuWidth/4)) {
                         mScroller.startScroll(getScrollX(), 0, -mMenuWidth - getScrollX(), 0, 300);
-                        //设置一个已经打开的标识，当实现点击开关自动打开关闭功能时会用到
                         isOpen = true;
-                        //一定不要忘了调用这个方法重绘，否则没有动画效果
+                        scrollEndScaleY();
                         invalidate();
-                    } else {//关闭Menu
-                        //同上
+                    } else {
+                        /**
+                         * 条件未达到，回弹
+                         */
                         mScroller.startScroll(getScrollX(), 0, -getScrollX(), 0, 300);
                         isOpen = false;
+                        scaleReset();
                         invalidate();
                     }
                 }
@@ -198,13 +222,28 @@ public class DragLayout extends ViewGroup {
     public void computeScroll() {
         if (mScroller.computeScrollOffset()){
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
-            //scale = Math.abs((float)getScrollX()) / (float) mMenuWidth;
-            //  mMenu.setTranslationX(2*(mMenuWidth+getScrollX())/3);
-            //mMenu.setTranslationX(mMenuWidth+getScrollX());
+            Log.i("qiyue","computeScroll");
+            mMenu.setTranslationX(2*(mMenuWidth+getScrollX())/3);
             invalidate();
         }
     }
 
+    private void scrollScaleY(){
+        float scrollXfloat = (float) getScrollX();
+        float mMenuWidthfloat = (float)mMenuWidth;
+        float percent = Math.abs((scrollXfloat/mMenuWidthfloat));
+        Log.i("qiyue","percent="+percent);
+        float f1 = (1 - (percent/4));
+        ViewHelper.setScaleY(mContent, f1);
+    }
+
+    private void scrollEndScaleY(){
+        ViewHelper.setScaleY(mContent, 0.75f);
+    }
+
+    private void scaleReset(){
+        ViewHelper.setScaleY(mContent, 1);
+    }
 
     /**
      * 将传进来的数转化为dp
@@ -231,5 +270,6 @@ public class DragLayout extends ViewGroup {
         invalidate();
         isOpen = true;
     }
+
 
 }
